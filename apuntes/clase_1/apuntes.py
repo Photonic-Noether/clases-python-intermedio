f13521671b2c9554d6f892fe8498ccddd03ee3bf
Joaquin Hernandez Martinez <starseeker.code@gmail.com>
2026-04-22 11:26:30 +0200

    main: README completo del curso, estructura limpia

--- apuntes/clase_1/apuntes.py
+++
@@ -1,5 +0,0 @@
-# Documentacion Poetry: https://python-poetry.org/
-# Documentacion Pandas: https://pandas.pydata.org/
-# Documentacion Polars: https://pola.rs/
-
-print("Hoy no nos dio tiempo a hacer mas!")
